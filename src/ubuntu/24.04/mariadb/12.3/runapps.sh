a42669f1f51d3955bdcde8e9883f3b6cdfebb9ab
charescape <53265646+charescape@users.noreply.github.com>
2026-08-29 09:46:24 +0800

    wwwdata

--- src/ubuntu/24.04/mariadb/12.3/runapps.sh
+++ src/ubuntu/24.04/mariadb/12.3/runapps.sh
@@ -4,4 +4,4 @@ set -eux
 
 # One-shot boot prepare. The daemon is started by runit: /etc/service/mariadb/run
 chown -R www-data:www-data /usr/local/mysql/
-chown -R www-data:www-data /data/mysql/
+chown -R www-data:www-data /wwwdata/mysql/
