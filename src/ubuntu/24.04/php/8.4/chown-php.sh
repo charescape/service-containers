70a1f0be437d2a90aa5b44e8fc922ce86b1d6483
charescape <53265646+charescape@users.noreply.github.com>
2026-08-30 15:11:25 +0800

    Update chown-php.sh

--- src/ubuntu/24.04/php/8.4/chown-php.sh
+++ src/ubuntu/24.04/php/8.4/chown-php.sh
@@ -5,3 +5,8 @@ set -eux
 # One-shot boot prepare. The daemon is started by runit: /etc/service/php-fpm/run
 chown -R www-data:www-data /usr/local/php/
 chown -R www-data:www-data /wwwdata/
+
+if [ -d /my_shared_dir ]; then
+  mkdir -p /my_shared_dir/repos
+  chown -R www-data:www-data /my_shared_dir
+fi
